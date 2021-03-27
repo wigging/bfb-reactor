f1905beea61363fa275d7c1c8bf5376126c3857b
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2021-03-26 21:27:09 -0400

    Plot gas and solid velocity

--- dyn-bfbgasf/plotter.py
+++ dyn-bfbgasf/plotter.py
@@ -59,75 +59,18 @@ def plot_h2(rhob_h2, t):
     _style_axis(ax)
 
 
-# v v v    outdated plots below here    v v v
+def plot_velocity(mfg, rhob_g, v, x):
+    ug = mfg / rhob_g
+    ux = np.concatenate(([ug[0, -1]], ug[:, -1], [ug[-1, -1]]))
+    vx = np.concatenate(([v[0, -1]], v[:, -1], [v[-1, -1]]))
 
-def plot_ts(results):
-    x = results['x']
-    t = results['t']
-    Ts = results['Ts']
-
-    # Ts along height of the reactor at final time
-    Tsx = np.concatenate(([Ts[0, -1]], Ts[:, -1], [Ts[-1, -1]]))
-
-    _, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, sharey=True, tight_layout=True)
-
-    ax1.plot(t, Ts[0], label='btm')
-    ax1.plot(t, Ts[50], label='mid')
-    ax1.plot(t, Ts[-1], label='top')
-    ax1.set_xlabel('t [s]')
-    ax1.set_ylabel('Ts [K]')
-    ax1.legend()
-    _style_axis(ax1)
-
-    ax2.plot(x, Tsx)
-    ax2.set_xlabel('x [m]')
-    _style_axis(ax2)
-
-
-def plot_tg(results):
-    x = results['x']
-    t = results['t']
-    Tg = results['Tg']
-
-    # Tg along height of the reactor at final time
-    Tgx = np.concatenate(([Tg[0, -1]], Tg[:, -1], [Tg[-1, -1]]))
-
-    _, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, sharey=True, tight_layout=True)
-
-    ax1.plot(t, Tg[0], label='btm')
-    ax1.plot(t, Tg[50], label='mid')
-    ax1.plot(t, Tg[-1], label='top')
-    ax1.set_xlabel('t [s]')
-    ax1.set_ylabel('Tg [K]')
-    ax1.legend()
-    _style_axis(ax1)
-
-    ax2.plot(x, Tgx)
-    ax2.set_xlabel('x [m]')
-    _style_axis(ax2)
-
-
-def plot_rhobb(results):
-    x = results['x']
-    t = results['t']
-    rhob_b = results['rhob_b']
-
-    # rhob_b along height of the reactor at final time
-    rhob_bx = np.concatenate(([rhob_b[0, -1]], rhob_b[:, -1], [rhob_b[-1, -1]]))
-
-    _, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, sharey=True, tight_layout=True)
-
-    ax1.plot(t, rhob_b[0], label='btm')
-    ax1.plot(t, rhob_b[50], label='mid')
-    ax1.plot(t, rhob_b[-1], label='top')
-    ax1.set_xlabel('t [s]')
-    ax1.set_ylabel('rhob_b [kg/m³]')
-    ax1.legend()
-    _style_axis(ax1)
-
-    ax2.plot(x, rhob_bx)
-    ax2.set_xlabel('x [m]')
-    _style_axis(ax2)
+    _, ax = plt.subplots(tight_layout=True)
+    ax.plot(x, ux, label='gas')
+    ax.plot(x, vx, label='solid')
+    ax.set_xlabel('Height [m]')
+    ax.set_ylabel('Velocity [m/s]')
+    ax.legend(loc='best')
+    _style_axis(ax)
 
 
 def show_plots():
